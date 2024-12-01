c31491490d16fdddd18182f818d89c6e39101f01
GuusvanderVegt <g.vandervegt@innovadis.com>
2024-12-01 14:37:40 +0100

    Cleanup

--- AdventOfCode.2024/Days/Day1.cs
+++ AdventOfCode.2024/Days/Day1.cs
@@ -13,12 +13,6 @@ public class Day1 : BaseDay
         
         var answerPart1 = leftList.Zip(rightList, (l, r) => Math.Abs(l - r)).Sum();
         var answerPart2 = leftList.Select(x => rightList.Count(y => y == x) * x).Sum();
-        // var answerPart2 = 0;
-        // foreach(var item in leftList)
-        // {
-        //     var occurenceAmount = rightList.Count(x => x == item);
-        //     answerPart2 += occurenceAmount * item;
-        // }
         
         PrintSolution(1, answerPart1.ToString());
         PrintSolution(2, answerPart2.ToString());
