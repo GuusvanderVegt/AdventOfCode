45c608fcc12f4ae41c0b050be68385a8438f7343
GuusvanderVegt <g.vandervegt@innovadis.com>
2024-12-01 14:35:45 +0100

    Finished day 1 for 2024

---
+++ AdventOfCode/BaseDay.cs
@@ -0,0 +1,30 @@
+﻿namespace AdventOfCode;
+
+public abstract class BaseDay
+{
+    protected BaseDay(string day)
+    {
+        // ReSharper disable once VirtualMemberCallInConstructor
+        Solve(GetLinesFromFile("Files/Day_" + day + ".txt"));
+    }
+
+    private static List<string> GetLinesFromFile(string fileName)
+    {
+        try
+        {
+            return File.ReadLines(fileName).ToList();
+        } 
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            return new List<string>();
+        }
+    }
+    
+    protected abstract void Solve(List<string> lines);
+    
+    protected static void PrintSolution(int part, string solution)
+    {
+        Console.WriteLine("Solution part " + part + ": " + solution);
+    }
+}
