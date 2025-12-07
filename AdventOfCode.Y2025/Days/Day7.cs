7a24492202f50063e338ac780fe66a85cb457fbf
GuusvanderVegt <g.vandervegt@innovadis.com>
2025-12-07 16:42:27 +0100

    Day 7 progress

--- AdventOfCode.Y2025/Days/Day7.cs
+++ AdventOfCode.Y2025/Days/Day7.cs
@@ -7,7 +7,37 @@ public class Day7 : BaseDay
 
     protected override void Solve(List<string> lines)
     {
+        var diagram = new char[lines.Count][];
+        for (var i = 0; i < lines.Count; i++)
+        {
+            var length = lines[i].Length;
+            diagram[i] = new char[length];
+            for (var j = 0; j < length; j++)
+            {
+                diagram[i][j] = lines[i][j];
+            }
+        }
         
+        var startY = lines[0].IndexOf('S');
+        List<int> beamYIndexes = [startY];
+        
+        // Start at 1 as we can skip the first row
+        for (var i = 1; i < lines.Count; i++)
+        {
+            var line = diagram[i];
+            var splitIndices = line.Select((c, index) => new { c, index })
+                .Where(x => x.c == '^')
+                .Select(x => x.index)
+                .ToList();
+
+            if (splitIndices.Count == 0)
+            {
+                foreach (var beamIndice in beamYIndexes)
+                {
+                    line[beamIndice] = '|';
+                }
+            }
+        }
     }
 
 }
