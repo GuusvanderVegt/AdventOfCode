b5a8988e67e1d193adca6fc6f02b0e330004a196
GuusvanderVegt <g.vandervegt@innovadis.com>
2025-12-08 08:54:36 +0100

    Finished day 07

--- AdventOfCode.Y2025/Days/Day7.cs
+++ AdventOfCode.Y2025/Days/Day7.cs
@@ -7,37 +7,88 @@ public class Day7 : BaseDay
 
     protected override void Solve(List<string> lines)
     {
-        var diagram = new char[lines.Count][];
-        for (var i = 0; i < lines.Count; i++)
-        {
-            var length = lines[i].Length;
-            diagram[i] = new char[length];
-            for (var j = 0; j < length; j++)
-            {
-                diagram[i][j] = lines[i][j];
-            }
-        }
-        
+        Part1(lines);
+        Part2(lines);
+    }
+
+    private static void Part1(List<string> lines)
+    {
         var startY = lines[0].IndexOf('S');
-        List<int> beamYIndexes = [startY];
+        HashSet<int> beamYIndexes = [startY];
+        var answerPart1 = 0;
         
         // Start at 1 as we can skip the first row
         for (var i = 1; i < lines.Count; i++)
         {
-            var line = diagram[i];
+            var line = lines[i];
             var splitIndices = line.Select((c, index) => new { c, index })
                 .Where(x => x.c == '^')
                 .Select(x => x.index)
+                .Where(index => beamYIndexes.Contains(index))
                 .ToList();
 
-            if (splitIndices.Count == 0)
+            foreach (var splitIndex in splitIndices)
             {
-                foreach (var beamIndice in beamYIndexes)
+                answerPart1++;
+
+                var prev = splitIndex - 1;
+                var next = splitIndex + 1;
+                if(prev > 0)
                 {
-                    line[beamIndice] = '|';
+                    beamYIndexes.Add(prev);
                 }
+
+                if (next < line.Length)
+                {
+                    beamYIndexes.Add(next);
+                }
+                
+                beamYIndexes.Remove(splitIndex);
             }
         }
+        
+        PrintSolution(1, answerPart1.ToString());
     }
-
+    
+    private static void Part2(List<string> lines)
+    {
+        var startY = lines[0].IndexOf('S');
+        Dictionary<int, long> pathsPerPosition = new() { [startY] = 1 };
+    
+        for (var i = 1; i < lines.Count; i++)
+        {
+            var line = lines[i];
+            Dictionary<int, long> newPathsPerPosition = new();
+    
+            foreach (var (position, pathCount) in pathsPerPosition)
+            {
+                if (line[position] == '^')
+                {
+                    // Split: distribute paths to left and right
+                    var left = position - 1;
+                    var right = position + 1;
+    
+                    if (left >= 0)
+                    {
+                        newPathsPerPosition[left] = newPathsPerPosition.GetValueOrDefault(left) + pathCount;
+                    }
+    
+                    if (right < line.Length)
+                    {
+                        newPathsPerPosition[right] = newPathsPerPosition.GetValueOrDefault(right) + pathCount;
+                    }
+                }
+                else
+                {
+                    // No split: carry forward all paths to same position
+                    newPathsPerPosition[position] = newPathsPerPosition.GetValueOrDefault(position) + pathCount;
+                }
+            }
+    
+            pathsPerPosition = newPathsPerPosition;
+        }
+    
+        var answerPart2 = pathsPerPosition.Values.Sum();
+        PrintSolution(2, answerPart2.ToString());
+    }
 }
